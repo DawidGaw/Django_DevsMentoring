e009356538e87e9907344a1e945cff99b76c3163
DawidGaw <dawidgaworski@onet.pl>
2026-02-22 13:53:08 +0100

    Article Management (CRUD) added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
+    path("", ArticleListView.as_view(), name="blog-home"),
     path("about/", views.about, name="blog-about"),
+    path("article/<int:pk>/", ArticleDetailView.as_view(), name="article-detail"),
+    path("article/new/", ArticleCreateView.as_view(), name="article-create"),
+    path("article/update/<int:pk>/", ArticleUpdateView.as_view(), name="article-update"),
+    path("article/delete/<int:pk>/", ArticleDeleteView.as_view(), name="article-delete"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from blog.models import Article
-
+from .models import Article
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 POSTS = [
     {
@@ -23,3 +26,49 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/article.html'
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        article = self.get_object()
+        return self.request.user == article.author
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = 'blog/article_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        article = self.get_object()
+        return self.request.user == article.author
+
+
+
+
